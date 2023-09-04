89ecdce571d5354449e265f494100e317b1b914a
Alex McKinney <alexmckinney01@gmail.com>
2023-09-04 14:07:16 -0400

    :herb: Add local generation configuration (#14)
    * Add local generation configuration
    
    * Add demo application in local/
    
    * Add local executable to .gitignore
    
    * Revise local generation setup

---
+++ local/go/go.mod
@@ -0,0 +1,7 @@
+module github.com/hookdeck/hookdeck-api-schema/local/go
+
+go 1.19
+
+require github.com/hookdeck/hookdeck-go-sdk v0.0.0
+
+replace github.com/hookdeck/hookdeck-go-sdk v0.0.0 => ./generated/hookdeck-go-sdk
---
+++ local/go/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	hookdeck "github.com/hookdeck/hookdeck-go-sdk"
+	hookdeckclient "github.com/hookdeck/hookdeck-go-sdk/client"
+)
+
+func main() {
+	if err := run(); err != nil {
+		os.Stderr.WriteString(err.Error())
+		os.Exit(1)
+	}
+}
+
+func run() error {
+	client := hookdeckclient.NewClient(
+		hookdeckclient.ClientWithBaseURL("http://localhost:9000/2023-07-01"),
+		hookdeckclient.ClientWithAuthToken(os.Getenv("HOOKDECK_AUTH_TOKEN")),
+	)
+	issueTriggersResponse, err := client.IssueTrigger.List(
+		context.TODO(),
+		new(hookdeck.IssueTriggerListRequest),
+	)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("Got %d issue triggers\n", len(issueTriggersResponse.Models))
+	return nil
+}
